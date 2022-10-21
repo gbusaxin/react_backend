4f935700ee5c59156250d30ed5fb1a2243a09dee
GBu <grigorii.busakhin@software-foundation.sk>
2022-10-21 09:58:38 +0200

    Initial commit

---
+++ src/main/java/gbusaxin/flux/project/MyWebFluxProjectApplication.java
@@ -0,0 +1,13 @@
+package gbusaxin.flux.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebFluxProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWebFluxProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gbusaxin/flux/project/MyWebFluxProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package gbusaxin.flux.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebFluxProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
